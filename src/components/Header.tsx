63772b85e29aa0eed77a88efe3cc9e5c2b5e7318
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-05-30 23:48:30 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,54 +18,56 @@ export function Header() {
   return (
     <div className="w-full">
       <NavigationMenu className="pt-8">
-        <div className="px-6 md:items-center md:flex">
-          <div className="flex items-center justify-start py-3">
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <Image src={Logo} alt="Logo" className="mx-5"  />
-              </NavigationMenuItem>
-              <div className="md:hidden">
-                <button
-                  className="p-2 fixed top-8 right-4 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-black"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-black"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  )}
-                </button>
-              </div>
-            </NavigationMenuList>
+        <div className="px-6 md:items-center md:flex w-full md:w-fit justify-between">
+          <div className="flex items-center py-3">
+            <div className="w-full">
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <Image src={Logo} alt="Logo" className="mx-5" />
+                </NavigationMenuItem>
+                <div className="md:hidden">
+                  <button
+                    className="p-2  text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {navbar ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6 text-black"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6 text-black"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    )}
+                  </button>
+                </div>
+              </NavigationMenuList>
+            </div>
           </div>
 
           <div>
             <div
-              className={`pb-4 mt-4 md:block md:pb-0 md:mt-0 ${
+              className={`h-[90vh] md:h-fit w-full flex justify-center items-center  pb-4 mt-4 md:block md:pb-0 md:mt-0 ${
                 navbar ? "block" : "hidden"
               }`}
             >
@@ -110,61 +112,3 @@ export function Header() {
     </div>
   );
 }
-
-// "use client";
-
-// import {
-//   NavigationMenu,
-//   NavigationMenuItem,
-//   NavigationMenuList,
-// } from "@/components/ui/navigation-menu";
-// import { AiOutlineShoppingCart } from "react-icons/ai";
-// import { BiSearch } from "react-icons/bi";
-// import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
-// import Image from "next/image";
-// import Logo from "/public/logo.png";
-
-// export function Header() {
-//   return (
-//     <NavigationMenu className="pt-8">
-//       <NavigationMenuList>
-//         <NavigationMenuItem>
-//           <Image src={Logo} alt="Logo" className="mr-5" />
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <p>Female</p>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <p>Male</p>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <p>Kids</p>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <p>All Products</p>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <div className="flex border rounded-lg border-black h-8 items-center">
-//             <Button className="bg-white text-black hover:bg-white hover:text-black h-6 pr-1.5 mr-0">
-//               <BiSearch />
-//             </Button>
-//             <Input
-//               type="text"
-//               placeholder="What you looking for"
-//               className="border-none h-6"
-//             />
-//           </div>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <Button className="bg-gray-200 text-black hover:bg-gray-300 hover:text-black  text-xl rounded-full p-2.5">
-//             <AiOutlineShoppingCart />
-//             <span className="cart-item-count absolute top-0.5 right-0 bg-red-500 text-white text-xs rounded-full h-3 w-3 flex items-center justify-center">
-//               1
-//             </span>
-//           </Button>
-//         </NavigationMenuItem>
-//       </NavigationMenuList>
-//     </NavigationMenu>
-//   );
-// }
