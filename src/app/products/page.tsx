9f499b17dbcaf2c00809f254f32abc3d9cbe16d4
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-06-12 03:23:29 +0500

    fetching original products from sanity

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,22 +1,44 @@
 import Image from "next/image";
-import p1_img from "/public/prod1.png";
+import type { Image as IImage } from "sanity";
+import { client } from "../../../sanity/lib/client";
+import { urlForImage } from "../../../sanity/lib/image";
 
-const page = () => {
+interface Product {
+  title: string;
+  category: string;
+  price: string;
+  description: string;
+  image: IImage;
+}
+
+async function getData() {
+  let res = await client.fetch(`*[_type=="product"]`);
+  return res;
+}
+
+const page = async () => {
+  let data = await getData();
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 my-16 px-10 md:px-24 lg:px-32 gap-x-5">
-        <div className="h-fit">
-          <Image
-            src={p1_img}
-            alt="Logo"
-            className="object-cover w-full h-full max-h-64 md:max-h-80"
-          />
-          <h2 className="font-bold tracking-wider leading-6 mt-2">
-            Brushed Raglan Sweatshirt
-          </h2>
-          <h2 className="font-bold text-[#D7D7D9]">Sweater</h2>
-          <h2 className="text-lg font-semibold my-1">$292</h2>
-        </div>
+        {data.map((item: Product) => {
+          return (
+            <div className="h-fit">
+              <Image
+                src={urlForImage(item.image).url()}
+                alt="Logo"
+                width={300}
+                height={300}
+                className="object-cover w-full h-full max-h-64 md:max-h-80"
+              />
+              <h2 className="font-bold tracking-wider leading-6 mt-2">
+                {item.title}
+              </h2>
+              <h2 className="font-bold text-[#D7D7D9]">{item.category}</h2>
+              <h2 className="text-lg font-semibold my-1">${item.price}</h2>
+            </div>
+          );
+        })}
       </div>
     </>
   );
