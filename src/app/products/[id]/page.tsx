4d9d2a7bae75e3efd9792a4741af4f65e381757b
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-06-14 12:14:51 +0500

    product care field show in UI

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -15,6 +15,7 @@ interface Product {
   description: string;
   image: IImage;
   image_thumbnail: IImage;
+  product_care: string[];
 }
 
 async function getData(id: string) {
@@ -24,23 +25,37 @@ async function getData(id: string) {
 
 const page = async ({ params }: { params: { id: string } }) => {
   let id = params.id;
-  let data:Product = await getData(id);
+  let data: Product = await getData(id);
 
   return (
     <>
       <div className="bg-[#FCFCFC] my-5 py-16 px-10 md:px-24 lg:px-32">
         <div className="w-full flex flex-wrap">
           <div className="w-[30%] md:w-[15%] px-5">
-            <Image src={urlForImage(data.image_thumbnail).url()} width={200} height={200} alt="Product" className="w-full" />
+            <Image
+              src={urlForImage(data.image_thumbnail).url()}
+              width={200}
+              height={200}
+              alt="Product"
+              className="w-full"
+            />
           </div>
           <div className="w-[70%] md:w-[55%]">
-            <Image src={urlForImage(data.image).url()} width={500} height={500} alt="Product" className="w-full" />
+            <Image
+              src={urlForImage(data.image).url()}
+              width={500}
+              height={500}
+              alt="Product"
+              className="w-full"
+            />
           </div>
           <div className="w-[100%] md:w-[30%] px-4 space-y-7 py-16">
             <h2 className="text-2xl tracking-wider">
               {data.title}
               <br />
-              <span className="text-lg font-bold text-[#D7D7D9]">{data.category}</span>
+              <span className="text-lg font-bold text-[#D7D7D9]">
+                {data.category}
+              </span>
             </h2>
             <div className="flex items-center">
               <h4 className="font-bold pr-5">Quantity:</h4>
@@ -72,17 +87,16 @@ const page = async ({ params }: { params: { id: string } }) => {
             <div className="col-span-2 text-justify tracking-wide leading-7">
               {data.description}
             </div>
-            {/* <div className="col-span-1">
+            <div className="col-span-1">
               <h2 className="font-bold text-gray-600">PRODUCT CARE</h2>
             </div>
             <div className="col-span-2">
               <ul className="list-disc ml-4 font-semibold">
-                <li>Hand wash using cold water.</li>
-                <li>Do not using bleach.</li>
-                <li>Hang it to dry.</li>
-                <li>Iron on low temperature.</li>
+                {data.product_care.map((item, i) => {
+                  return <li key={i}>{item}</li>;
+                })}
               </ul>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
