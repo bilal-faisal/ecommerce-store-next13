63e317506fbe3089ee9f85f4ee82aafce5ffb2cf
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-06-17 02:02:50 +0500

    update schema

---
+++ sanity/categorySchema.ts
@@ -0,0 +1,12 @@
+export const categorySchema = {
+    name: "category",
+    type: "document",
+    title: "Category",
+    fields: [
+        {
+            name: "name",
+            title: "Product Category",
+            type: "string"
+        }
+    ]
+}
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { productSchema } from './productSchema'
+import { categorySchema } from './categorySchema'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema],
+  types: [productSchema, categorySchema],
 }
