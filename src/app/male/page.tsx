ce606a9908dc53c1eea199b355af042b67277d13
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-06-19 02:42:00 +0500

    Update male/page.tsx

--- src/app/male/page.tsx
+++ src/app/male/page.tsx
@@ -27,30 +27,28 @@ async function getMaleData() {
 const page = async () => {
   let data = await getMaleData();
   return (
-    <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 my-16 px-10 md:px-24 lg:px-32 gap-x-5">
-        {data.map((item: Product, i: number) => {
-          return (
-            <div className="h-fit" key={i}>
-              <Link href={`/products/${item._id}`}>
-                <Image
-                  src={urlForImage(item.image_thumbnail).url()}
-                  alt="Logo"
-                  width={300}
-                  height={300}
-                  className="w-full h-full max-h-64 md:max-h-64 object-cover object-top"
-                />
-                <h2 className="font-bold tracking-wider leading-6 mt-2">
-                  {item.title}
-                </h2>
-                <h2 className="font-bold text-[#D7D7D9]">{item.type}</h2>
-                <h2 className="text-lg font-semibold my-1">${item.price}</h2>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 my-16 px-10 md:px-24 lg:px-32 gap-x-5">
+      {data.map((item: Product, i: number) => {
+        return (
+          <div className="h-fit" key={i}>
+            <Link href={`/products/${item._id}`}>
+              <Image
+                src={urlForImage(item.image_thumbnail).url()}
+                alt="Logo"
+                width={300}
+                height={300}
+                className="w-full h-full max-h-64 md:max-h-64 object-cover object-top"
+              />
+              <h2 className="font-bold tracking-wider leading-6 mt-2">
+                {item.title}
+              </h2>
+              <h2 className="font-bold text-[#D7D7D9]">{item.type}</h2>
+              <h2 className="text-lg font-semibold my-1">${item.price}</h2>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
