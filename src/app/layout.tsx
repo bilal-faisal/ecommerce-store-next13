baf9da619b529512a117f0187abd2805d32c718c
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-07-21 21:51:47 +0500

    redux implemented with counter as example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
-        <Header />
-        {children}
-        <Footer />
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
---
+++ src/app/reduxExample/page.tsx
@@ -0,0 +1,12 @@
+import Counter from "./Counter";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default page;
